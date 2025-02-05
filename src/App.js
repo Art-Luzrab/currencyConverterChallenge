ba289780360db93ad29bf924e539f49a8610afeb
Arthur <arthur91021@gmail.com>
2025-02-05 08:13:51 -0800

    added starter files

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [outPut, setOutPut] = useState("");
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
+
+// useEffect(function () {
+// }, [])
